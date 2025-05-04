9f25063e7b6c5cd39a707702a9603ed8bd3e9152
Kondratenko Mikhailo <mike.kondr@gmail.com>
2025-05-04 22:20:01 +0300

    finish

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CommonControlsApp
 {
@@ -14,7 +16,31 @@ namespace CommonControlsApp
 
         private void MenuItem_New_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Створити новий");
+        }
 
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Відкрити");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContextMenu cm = this.FindResource("cmButton") as ContextMenu;
+            cm.PlacementTarget = sender as Button;
+            cm.IsOpen = true;
+        }
+
+        private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
+            int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
+            lblCursorPosition.Text = "Рядок " + (row + 1) + ", Стовпець " + (col + 1);
         }
     }
 }
