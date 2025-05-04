1459492474594d2972667be3dd25987ef67c2d95
Kondratenko Mikhailo <mike.kondr@gmail.com>
2025-05-04 21:01:21 +0300

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CommonControlsApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_New_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
